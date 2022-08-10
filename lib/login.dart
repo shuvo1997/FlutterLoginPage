8affa4ee9dd7e87a06f4eaf5feb614e3fb26036c
BS983 <shuvo1997@yahoo.com>
2022-08-10 12:23:25 +0600

    Adds Form Validation

--- lib/login.dart
+++ lib/login.dart
@@ -12,105 +12,128 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10),
-              child: const Text(
-                'My Awesome App',
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic),
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Login ',
-                    style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Icon(
-                    Icons.login,
-                    color: Colors.blue,
-                  )
-                ],
-              ),
-            ),
-            Container(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: [
+              Container(
                 alignment: Alignment.center,
                 padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: usernameController,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Username'),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                child: TextField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Password'),
-                )),
-            TextButton(
-                onPressed: () {
-                  print('Clicked forgot password');
-                },
-                child: const Text('Forgot Password')),
-            Container(
-              height: 50,
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: ElevatedButton(
                 child: const Text(
-                  'Login',
-                  style: TextStyle(fontSize: 20),
+                  'My Awesome App',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
                 ),
-                onPressed: () {
-                  print(usernameController.text);
-                  print(passwordController.text);
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const NewPage()));
-                },
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Don\'t have account?',
-                  style: TextStyle(fontSize: 20),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Login ',
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Icon(
+                      Icons.login,
+                      color: Colors.blue,
+                    )
+                  ],
                 ),
-                TextButton(
+              ),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'please enter some text';
+                      }
+                      if (value == 'user') {
+                        return null;
+                      }
+                      return 'please enter correct username';
+                    },
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Username'),
+                  )),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextFormField(
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      if (value == 'password') {
+                        return null;
+                      }
+                      return 'Please enter correct password';
+                    },
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Password'),
+                  )),
+              TextButton(
+                  onPressed: () {
+                    print('Clicked forgot password');
+                  },
+                  child: const Text('Forgot Password')),
+              Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
                   child: const Text(
-                    'Sign Up',
+                    'Login',
                     style: TextStyle(fontSize: 20),
                   ),
                   onPressed: () {
-                    print('Clicked Sign Up Button');
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const NewPage()));
+                    }
                   },
-                )
-              ],
-            )
-          ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Don\'t have account?',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  TextButton(
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      print('Clicked Sign Up Button');
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
